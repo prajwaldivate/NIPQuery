718bc33555f24cc62eccd876c7f6b1d8a390e5a7
Prajwal K Divate <prajwaldivate@eoxvantage.com>
2025-03-25 14:10:24 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,16 +4,53 @@ SELECT
     s.name AS status_name,
     TIMESTAMPDIFF(DAY, ts.received_date, ts.end_timestamp) AS time_diff_days,
     TIMESTAMPDIFF(HOUR, ts.received_date, ts.end_timestamp) AS time_diff_hours,
+    -- Updated Rule Logic
     CASE
-        -- For "Completed" status: Check time between start and end timestamps
-        WHEN s.id = 5 AND 
-             TIMESTAMPDIFF(HOUR, ts.start_timestamp, ts.end_timestamp) <= p.expected_tat_hrs 
-             THEN 'Green'
-        -- For pending statuses: Check time from start to NOW()
-        WHEN s.id IN (10,6) AND 
-             TIMESTAMPDIFF(HOUR, ts.start_timestamp, NOW()) <= p.expected_tat_hrs 
-             THEN 'Green'
-        ELSE 'Red'
+        -- "Completed" Status (s.id = 5)
+        WHEN s.id = 5 THEN 
+            CASE 
+                WHEN p.rule_type = 'Received Date' 
+                    AND TIMESTAMPDIFF(HOUR, ts.received_date, ts.end_timestamp) <= p.expected_tat_hrs 
+                THEN 'Green'
+
+                WHEN p.rule_type = 'Date Received' 
+                    AND TIMESTAMPDIFF(HOUR, ts.start_timestamp, ts.end_timestamp) <= p.expected_tat_hrs 
+                THEN 'Green'
+                
+                WHEN p.rule_type = 'Effective Date in Future' 
+                    AND TIMESTAMPDIFF(HOUR, ts.end_timestamp, ts.effective_date) > p.expected_tat_hrs 
+                THEN 'Green'
+
+                WHEN p.rule_type = 'Effective Date in Past' 
+                    AND TIMESTAMPDIFF(HOUR, ts.effective_date, ts.end_timestamp) > p.expected_tat_hrs 
+                THEN 'Green'
+
+                ELSE 'Red'
+            END
+
+        -- "Not Completed" or "Pending Additional Information" (s.id IN (10,6))
+        WHEN s.id IN (10,6) THEN 
+            CASE
+                WHEN p.rule_type = 'Received Date' 
+                    AND TIMESTAMPDIFF(HOUR, ts.received_date, NOW()) <= p.expected_tat_hrs 
+                THEN 'Green'
+
+                WHEN p.rule_type = 'Date Received' 
+                    AND TIMESTAMPDIFF(HOUR, ts.start_timestamp, NOW()) <= p.expected_tat_hrs 
+                THEN 'Green'
+
+                WHEN p.rule_type = 'Effective Date in Future' 
+                    AND TIMESTAMPDIFF(HOUR, NOW(), ts.effective_date) > p.expected_tat_hrs 
+                THEN 'Green'
+
+                WHEN p.rule_type = 'Effective Date in Past' 
+                    AND TIMESTAMPDIFF(HOUR, ts.effective_date, NOW()) > p.expected_tat_hrs
+                THEN 'Green'
+
+                ELSE 'Red'
+            END
+
+        ELSE 'Ignore'
     END AS rule_status
 FROM
     timesheets ts
@@ -24,11 +61,7 @@ JOIN processes p
 WHERE
     ts.project_id = 29
     AND ts.record_type = 1
+    AND ts.received_date BETWEEN '{{ $json.body.recieved_date_from }}' AND '{{ $json.body.recieved_date_to }} 23:59:59'
     AND ts.received_date IS NOT NULL
     AND ts.end_timestamp IS NOT NULL
-    AND ts.end_timestamp <> '0000-00-00 00:00:00'
-    AND ts.received_date <> '0000-00-00 00:00:00'
     AND s.id IN (5,6,10)
-    AND p.id NOT IN (
-        593, 596, 645, 646, 647, 648, 677
-    );
